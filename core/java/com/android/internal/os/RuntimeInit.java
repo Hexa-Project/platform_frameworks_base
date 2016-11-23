d28c244f4bc044116c52f5e1afc7e9f098f35132
Danny Baumann <dannybaumann@web.de>
2016-11-23 10:12:35 +0100

    Make Build.TYPE and Build.FINGERPRINT consistent for apps
    Some apps (namely Android Wear) like to do comparisons between TYPE and
    FINGERPRINT and throw errors on inconsistencies. As our fingerprints are
    almost always taken from stock ROMs, they don't really match our builds,
    causing said comparisons to fail. Avoid those failures by taking build
    type out of fingerprint for apps.
    
    Change-Id: I1d4fbeea38331b48448e81a075fe9cf2c8fdd846

--- core/java/com/android/internal/os/RuntimeInit.java
+++ core/java/com/android/internal/os/RuntimeInit.java
@@ -200,6 +200,8 @@ public class RuntimeInit {
 
         Build.adjustBuildTypeIfNeeded();
 
+        Build.adjustBuildTypeIfNeeded();
+
         /*
          * Install a TimezoneGetter subclass for ZoneInfo.db
          */
